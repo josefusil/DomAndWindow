08c612473bfe8f2ea48cd2415407e3f43bdb18d4
jose fusil <josealejandrofusil2007@gmail.com>
2023-11-19 10:50:30 -0500

    nuevasPruebas

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ text.style.textAlign = 'center'
 const new_text = document.querySelectorAll('.text-diferent')
 console.log(new_text)
 
-let pedirColor = prompt("Elige el color de los ultimos dos parrafos")
-for (let iterador of new_text) {
-    iterador.style.color = pedirColor
+let pedirColor = confirm("Quieres elegir el color de los ultimos dos textos ?")
+if (pedirColor === true) {
+    let color = prompt("cual color quieres?")
+    for (let iterador of new_text) {
+        iterador.style.color = color
+    }
+} else {
+    for (let iterador of new_text) {
+        iterador.style.color = 'green'
+    }
 }
+text.innerHTML = '<p>Ahora ya no soy un encabezado</p>'
